dc9f7fef54a56fbf72796840c2c632a69c4f832a
Stasenko-Komstantin <stasenko.kost@yandex.ru>
2024-08-01 00:00:39 +0500

    fix token idk

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -3,6 +3,8 @@ module Lexer where
 import Token
 import qualified Data.Text as T
 
-scan :: T.Text -> [Token.Token]
-scan "" = []
-scan t = []
+scan :: T.Text -> [Token]
+scan text = help [] text
+  where
+    help :: [Token] -> T.Text -> [Token]
+    help tokens "" = reverse $ mkToken EOF (-1, -1) : tokens
--- src/Token.hs
+++ src/Token.hs
@@ -2,7 +2,7 @@ module Token where
 
 import qualified Data.Text as T
 
-data TokenType = TNum | TName | TString | TLParen | TRParen deriving (Show, Eq)
+data TokenType = TNum | TName | TString | TLParen | TRParen | EOF deriving (Show, Eq)
 
 data Token = Token
   { tokenType :: TokenType
@@ -16,3 +16,7 @@ instance Eq Token where
     if (tokenType left) /= (tokenType right)
     then False
     else (content left) == (content right)
+
+mkToken :: TokenType -> (Int, Int) -> Token
+mkToken tt (x, y) = Token tt (T.pack $ show tt) x y
+
